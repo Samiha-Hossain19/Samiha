b857e4ed3fa88021f1c0d0b74326596deb94cc82
Samiha Hossain <75538711+Samiha-Hossain19@users.noreply.github.com>
2023-06-05 17:44:47 +0600

    first commit

--- script.js
+++ script.js
@@ -81,20 +81,36 @@ function showData() {
 
   var html = "";
   table.forEach(function (element, index) {
-    html += "<tr>";
-    html += "<tr>" + element.category + "</tr>";
-    html += "<tr>" + element.title + "</tr>";
-    html += "<tr>" + element.url + "</tr>";
+    html += "<h5>" + element.category + "</h5>";
+    html += "<p>" + element.title + "</p>";
+    html += '<button onclick="showData2( ' + ')"> Details</button>';
     html +=
-      '<td> <button onclick="deleteData(' +
+      '<p> <button onclick="deleteData(' +
       index +
-      ')" class="btn btn-danger">Delete</button></td>';
-    html += "</tr>";
+      ')" class="btn btn-danger">Delete</button></p>';
   });
-  document.querySelector("#crudTable tbody").innerHTML = html;
+  document.querySelector("#cardContainer").innerHTML = html;
 }
 
 document.onload = showData();
+
+//deatils show data
+function showData2() {
+  var table;
+  if (localStorage.getItem("table") == null) {
+    table = [];
+  } else {
+    table = JSON.parse(localStorage.getItem("table"));
+  }
+  var html = "";
+  table.forEach(function (element, index) {
+    html += "<p>" + element.title + "</p>";
+    html += "<p>" + element.category + "</p>";
+    html += "<p>" + element.url + "</p>";
+  });
+  document.querySelector("#dets p").innerHTML = html;
+}
+
 //Add Data
 function AddData() {
   if (valiadateForm() == true) {
